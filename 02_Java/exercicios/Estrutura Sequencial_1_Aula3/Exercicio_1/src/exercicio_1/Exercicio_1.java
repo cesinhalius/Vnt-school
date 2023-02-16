f0b4450bf8d9d15d65ff8460fc686c550c9c236f
Cesar Augusto <cesinhalius@hotmail.com>
2023-02-16 20:00:07 -0300

    Adicionado o primeiro exercício da primeira lista de exercicio de Java

---
+++ 02_Java/exercicios/Estrutura Sequencial_1_Aula3/Exercicio_1/src/exercicio_1/Exercicio_1.java
@@ -0,0 +1,35 @@
+
+package exercicio_1;
+import java.util.Scanner;
+
+/* Faça um algoritmo para ler a base e a altura de um triangulo retângulo. 
+   Em seguida, calcular a sua área. Imprimir: base, altura e aárea.
+*/
+
+public class Exercicio_1 {
+
+   
+    public static void main(String[] args) {
+        double base;
+        double altura;
+        Scanner n = new Scanner(System.in);
+        double area;
+        System.out.println("Informe o valor da base: ");
+        base = n.nextDouble();
+        System.out.println("Informe o valor da altura: ");
+        altura = n.nextDouble();
+        
+        area = (base * altura) / 2;
+        
+        System.out.println("A base = " + base);
+        System.out.println("A altura = " + altura);
+        System.out.println("A area = " + area);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
