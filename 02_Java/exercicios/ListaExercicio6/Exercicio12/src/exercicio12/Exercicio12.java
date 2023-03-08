240b353fd69edcaa1ce1d1060096aa4068549494
Cesar Augusto <cesinhalius@hotmail.com>
2023-03-08 16:38:56 -0300

    Arrumando a lógica do programa

--- 02_Java/exercicios/ListaExercicio6/Exercicio12/src/exercicio12/Exercicio12.java
+++ 02_Java/exercicios/ListaExercicio6/Exercicio12/src/exercicio12/Exercicio12.java
@@ -18,23 +18,30 @@ public class Exercicio12 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Entre com um valor para I: ");
         int I = sc.nextInt();
-
-        switch (I) {
-            default:
-                System.out.println("Nao tem opcao com esse numero para I");
-                break;
-            case 1:
-                System.out.println("Entre com valor para A:");
+        
+         System.out.println("Entre com valor para A:");
                 double A = sc.nextDouble();
                 System.out.println("Entre com valor para B:");
                 double B = sc.nextDouble();
                 System.out.println("Entre com valor para C:");
                 double C = sc.nextDouble();
                 double aux;
+
+        switch (I) {
+            default:
+                System.out.println("Nao tem opcao com esse numero para I");
+                break;
+            case 1:
+               
                 if (A > B && A > C) {
                     aux = A;
                     A = C;
                     C = aux;
+                    if(A > B){
+                        aux = A;
+                        A = B;
+                        B = aux;
+                    }
                     System.out.println(A + "," + B + "," + C);
                 } else {
                     if (B > A && B > C) {
@@ -55,51 +62,46 @@ public class Exercicio12 {
                 }
                 break;
             case 2:
-                System.out.println("Entre com valor para A:");
-                double Z = sc.nextDouble();
-                System.out.println("Entre com valor para B:");
-                double Y = sc.nextDouble();
-                System.out.println("Entre com valor para C:");
-                double X = sc.nextDouble();
-                if (Z < Y && Z < X) {
-                    aux = Z;
-                    Z = X;
-                    X = aux;
-                    System.out.println(Z + "," + Y + "," + X);
+                
+                if (A < B && A < C) {
+                    aux = A;
+                    A = C;
+                    C = aux;
+                    System.out.println(A + "," + B + "," + C);
                 } else {
-                    if (Y < Z && Y < X) {
-                        aux = Y;
-                        Y = X;
-                        X = aux;
-                        System.out.println(Z + "," + Y + "," + X);
+                    if (B < A && B < C) {
+                        aux = B;
+                        B = C;
+                        C = aux;
+                    if(A < B){
+                        aux = A;
+                        A = B;
+                        B = aux;
+                    }    
+                        System.out.println(A + "," + B + "," + C);
                     } else {
-                        if (X < Z && X < Y) {
-                            System.out.println(Z + "," + Y + "," + X);
+                        if (C < A && C < B) {
+                            System.out.println(A + "," + B + "," + C);
                         }
                     }
                 }
                 break;
             case 3:
-                System.out.println("Entre com valor para A:");
-                double J = sc.nextDouble();
-                System.out.println("Entre com valor para B:");
-                double H = sc.nextDouble();
-                System.out.println("Entre com valor para C:");
-                double G = sc.nextDouble();
-                if (J > H && J > G) {
-                    aux = J;
-                    J = H;
-                    H = aux;
-                    System.out.println(J + "," + H + "," + G);
+                
+                if (A > B && A > C) {
+                    aux = A;
+                    A = B;
+                    B = aux;
+                    System.out.println(A + "," + B + "," + C);
                 } else {
-                    if (H > J && H > G) {
-                        System.out.println(J + "," + H + "," + G);
+                    if (B > A && B > C) {
+                        System.out.println(A + "," + B + "," + C);
                     } else {
-                        if (G > J && G > H) {
-                            aux = H;
-                            H = G;
-                            G = aux;
-                            System.out.println(J + "," + H + "," + G);
+                        if (C > A && C > B) {
+                            aux = B;
+                            B = C;
+                            C = aux;
+                            System.out.println(A + "," + B + "," + C);
                         }
                     }
                 }
