9182b2fd4e2dccfae29da52e49e2abfe2edb1e4a
Cesar Augusto <cesinhalius@hotmail.com>
2023-03-08 15:29:29 -0300

    Adcionando numero em ordem crescente

--- 02_Java/exercicios/ListaExercicio6/Exercicio9/src/exercicio9/Exercicio9.java
+++ 02_Java/exercicios/ListaExercicio6/Exercicio9/src/exercicio9/Exercicio9.java
@@ -13,20 +13,21 @@ public class Exercicio9 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Entre com um valor: ");
         int num1 = sc.nextInt();
         System.out.print("Entre com outro valor: ");
         int num2 = sc.nextInt();
-        
-        int resto = num1%num2;
-        
-         if (resto == 0) {
+
+        int resto = num1 % num2;
+        int resto2 = num2 % num1;
+
+        if (resto == 0 || resto2 == 0) {
             System.out.println("Sao Multiplos");
         } else {
             System.out.println("Nao sao Multiplos");
         }
-         sc.close();
+        sc.close();
 
     }
 
