8d700918c44ba6c62242dc10b6e4f6768eca193e
Cesar Augusto <cesinhalius@hotmail.com>
2023-03-08 22:47:41 -0300

    adicionando correto as validação

--- 02_Java/Aula5/Exercicio2/src/exercicio2/Exercicio2.java
+++ 02_Java/Aula5/Exercicio2/src/exercicio2/Exercicio2.java
@@ -22,27 +22,27 @@ public class Exercicio2 {
 
         //d)verificacao=12==11&&23==24;
         verificacao = 12;
-        String z = verificacao == 12 && 20 == 20 ? "true" : "false";
+        String z = verificacao == 12 && 23 == 24 ? "true" : "false";
         System.out.println(z);
 
         //e)verificacao=10==10||10==20;
         verificacao = 10;
-        String x = verificacao == 10 && verificacao == 20 ? "true" : "false";
+        String x = verificacao == 10 || 10 == 20 ? "true" : "false";
         System.out.println(x);
 
         //f)verificacao=11==12||20==20;
         verificacao = 11;
-        String y = verificacao == 12 && 20 == 20 ? "true" : "false";
+        String y = verificacao == 12 || 20 == 20 ? "true" : "false";
         System.out.println(r);
 
         // g)verificacao=15==15||16==16;
         verificacao = 15;
-        String c = verificacao == 15 && 16 == 16 ? "true" : "false";
+        String c = verificacao == 15 || 16 == 16 ? "true" : "false";
         System.out.println(c);
 
         //h)verificacao=12==11||23==24;
         verificacao = 12;
-        String d = verificacao == 11 && 23 == 24 ? "true" : "false";
+        String d = verificacao == 11 || 23 == 24 ? "true" : "false";
         System.out.println(r);
 
     }
