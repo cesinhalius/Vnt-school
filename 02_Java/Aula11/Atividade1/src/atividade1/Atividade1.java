707259f7ebfc38a3ca6eae06bb4507b1fa1b3293
Cesar Augusto <cesinhalius@hotmail.com>
2023-03-18 00:40:04 -0300

    Trocando os valores do exercicio

--- 02_Java/Aula11/Atividade1/src/atividade1/Atividade1.java
+++ 02_Java/Aula11/Atividade1/src/atividade1/Atividade1.java
@@ -1,18 +1,24 @@
 package atividade1;
 
+import java.util.Scanner;
+
 /**
  *
  * Crie um algoritmo que leia um vetor de tamanho 4.Inicialize o vetor com o
  * usuário digitando os valores de cada elemento. Inicie a soma desse vetor com
- * uma variável num=10.
+ * uma variável num = 10.
  */
 public class Atividade1 {
 
     public static void main(String[] args) {
-        int[] v = {1,2,3,4};
+        Scanner sc = new Scanner(System.in);
+        int[] v = new int[4];
         int num = 10;
         
         for (int i = 0; i < v.length; i++) {
+            System.out.println("Entre com um numero: ");
+            v[i] = sc.nextInt();
+            
             num = num + v[i];
             
             System.out.println("Elemento " + i + " : " + num);
