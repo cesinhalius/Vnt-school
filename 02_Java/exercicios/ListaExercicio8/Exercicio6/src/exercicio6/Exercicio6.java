0ef60706f137c7cba5308dbaa4a2b813da412a3d
Cesar Augusto <cesinhalius@hotmail.com>
2023-03-19 14:07:29 -0300

    Adicionando o algoritmo do exercicio

--- 02_Java/exercicios/ListaExercicio8/Exercicio6/src/exercicio6/Exercicio6.java
+++ 02_Java/exercicios/ListaExercicio8/Exercicio6/src/exercicio6/Exercicio6.java
@@ -1,6 +1,5 @@
 package exercicio6;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -13,13 +12,23 @@ public class Exercicio6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] vetor = new int[10];
+        int aux;
 
         for (int i = 0; i < vetor.length; i++) {
             System.out.println("Entre com um numero :");
             vetor[i] = sc.nextInt();
         }
 
-        Arrays.sort(vetor);
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = i + 1; j < 10; j++) {
+                if(vetor[i] > vetor[j]){
+                    aux = vetor[i];
+                    vetor[i] = vetor[j];
+                    vetor[j] = aux;
+                }
+            }
+            
+        }
 
         System.out.println("Ordem crescente:    ");
 
