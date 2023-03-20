f390fb266e9f7a949653b60f1d1bd9db7e888614
Cesar Augusto <cesinhalius@hotmail.com>
2023-03-19 21:05:27 -0300

    Adicionando métodos para exercicio 6

--- 02_Java/exercicios/ListaExercicio8/Exercicio6/src/exercicio6/Exercicio6.java
+++ 02_Java/exercicios/ListaExercicio8/Exercicio6/src/exercicio6/Exercicio6.java
@@ -12,29 +12,42 @@ public class Exercicio6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] vetor = new int[10];
-        int aux;
 
         for (int i = 0; i < vetor.length; i++) {
             System.out.println("Entre com um numero :");
             vetor[i] = sc.nextInt();
         }
-
-        for (int i = 0; i < vetor.length; i++) {
-            for (int j = i + 1; j < 10; j++) {
-                if(vetor[i] > vetor[j]){
-                    aux = vetor[i];
-                    vetor[i] = vetor[j];
-                    vetor[j] = aux;
-                }
-            }
-            
-        }
+        
+        ordemCrescente(vetor);
 
         System.out.println("Ordem crescente:    ");
+        
+        imprimirVetor(vetor);
 
-        for (int i = 0; i < vetor.length; i++) {
-            System.out.print(vetor[i] + "  ");
-        }
         sc.close();
+    }
+
+    static void imprimirVetor(int[] v) {
+        for (int i = 0; i < v.length; i++) {
+            System.out.print(v[i] + "  ");
+
+        }
+        System.out.println();
+
+    }
+
+    static void ordemCrescente(int[] v) {
+        int aux;
+
+        for (int i = 0; i < v.length; i++) {
+            for (int j = i + 1; j < 10; j++) {
+                if (v[i] > v[j]) {
+                    aux = v[i];
+                    v[i] = v[j];
+                    v[j] = aux;
+                }
+            }
+
+        }
     }
 }
